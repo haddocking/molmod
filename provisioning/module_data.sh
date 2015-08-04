07ae7868bc9dec5ba8152ed7852a926e0000d04f
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2015-08-04 18:13:06 +0200

    Added support for data repository

---
+++ provisioning/module_data.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#
+# General Provisioning for data files
+# shared by several modules
+#
+export DEBIAN_FRONTEND=noninteractive # StackOverflow 500764
+echo "[+] Provisioning: Module Data"
+echo "[++] Downloading data for the molmod modules"
+git clone https://github.com/JoaoRodrigues/molmod-data.git /opt/data/
