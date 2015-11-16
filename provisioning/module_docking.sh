14027161415cc29008b44df2af47d4d821c7490e
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2015-11-16 01:36:20 +0000

    Added WebLogo to provisioning

--- provisioning/module_docking.sh
+++ provisioning/module_docking.sh
@@ -10,4 +10,9 @@ echo "[+] Provisioning: Docking module"
 # Clustal Omega
 echo "[++] Downloading & installing Clustal Omega"
 wget -q -O /opt/software/clustalo http://www.clustal.org/omega/clustalo-1.2.0-Ubuntu-32-bit > /dev/null
-ln -s /opt/software/clustalo /opt/bin
+ln -s /opt/software/clustalo /opt/bin/
+
+# WebLogo
+echo "[++] Downloading & installing WebLogo"
+git clone https://github.com/WebLogo/weblogo.git /opt/software/weblogo > /dev/null
+ln -s /opt/software/weblogo/weblogo /opt/bin/
